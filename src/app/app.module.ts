163df5f2bbbb675748104b5a644e1dcb19ee5791
sohelnadaf1 <sohel.nadaf@jkteck.com>
2022-08-29 15:55:22 +0530

    HttpClientModule imported

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
+import { CommandsService } from './commands.service';
 
 import { AppComponent } from './app.component';
 
@@ -8,9 +10,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [CommandsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
