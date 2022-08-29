ffe461c137ef17d69c95b0d889145263a28e03c9
sohelnadaf1 <sohel.nadaf@jkteck.com>
2022-08-29 15:54:47 +0530

    Commands Service injected in AppCompo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Commands } from './Commands';
+import { CommandsService } from './commands.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Commands_UI';
+  title = "this is api call"
+
+  commands:any;
+ constructor (private _Commands:CommandsService)
+ {
+  _Commands.GetCommands().subscribe((data)=>
+  {
+    console.warn(data);
+    this.commands = data;
+  }
+  );
+  
+ }
+
 }
